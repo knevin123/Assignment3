cc442f691a870fbaeb0a93a2098b84e1f51ed772
knevin123 <knevin1997@gmail.com>
2016-04-08 22:48:57 +0100

    menu 3

--- src/Startmenu.java
+++ src/Startmenu.java
@@ -1,10 +1,19 @@
 import processing.core.PApplet;
+import processing.core.PImage;
+import processing.core.PVector;
 
 public class Startmenu {
 	PApplet parent;
+	PImage normal;
+	PVector pos;
+	boolean up;
 	Startmenu(PApplet p)
 	{
 		parent=p;
+		normal=parent.loadImage("sprite2.png");
+		pos = new PVector(0, 0);
+		pos.x=parent.width/2;
+		pos.y=(float) (parent.height/1.35);
 	}
 	void render()
 	{
@@ -17,6 +26,33 @@ public class Startmenu {
 		parent.rect((float) (parent.width/1.75)-75,(parent.height/2)-68,parent.width, 94);
 		parent.fill(255);
 		parent.text("Hoopers", (float) (parent.width/1.75), (float) (parent.height/2));
+		
+		parent.image(normal,pos.x-50,pos.y,100,110);
+	}
+	void update()
+	{
+		pos.x++;
+		if(pos.y<parent.height/1.35-80)
+		{
+			up=false;
+		}
+		if(pos.y>parent.height/1.35+80)
+		{
+			up=true;
+		}
+		
+		if(up==true)
+		{
+			pos.y-=2;
+		}
+		else
+		{
+			pos.y+=2;
+		}
+		if(pos.x>parent.width)
+		{
+			pos.x=0;
+		}
 	}
 
 }
--- src/Test.java
+++ src/Test.java
@@ -95,6 +95,7 @@ public class Test extends PApplet {
 //	    topwalldetect();
 //	    bottomwalldetect();
 	    start.render();
+	    start.update();
 	}
 	public void topwalldetect()
 	{
