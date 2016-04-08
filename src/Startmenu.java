ed9553b1237635896a1fec6639782552e5f8261c
knevin123 <knevin1997@gmail.com>
2016-04-08 21:47:44 +0100

    menu 2

--- src/Startmenu.java
+++ src/Startmenu.java
@@ -13,7 +13,10 @@ public class Startmenu {
 		parent.rect(0,parent.height/3-64, (float) (parent.width/1.5), 94);
 		parent.fill(255);
 		parent.text("Space", parent.width/4, parent.height/3);
-		
+		parent.fill(130);
+		parent.rect((float) (parent.width/1.75)-75,(parent.height/2)-68,parent.width, 94);
+		parent.fill(255);
+		parent.text("Hoopers", (float) (parent.width/1.75), (float) (parent.height/2));
 	}
 
 }
